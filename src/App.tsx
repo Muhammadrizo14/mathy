ff094827524e1699494a3252eb23dbb2d3ffe9b0
Muhammadrizo <mtursunboev13@gmail.com>
2024-03-19 23:43:58 +0500

    init, add toggle theme

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss'
+import Header from "./Layouts/Header";
+import Footer from "./Layouts/Footer";
+
+function App() {
+
+  return (
+    <div className='main'>
+      <Header/>
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Layouts/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss'
+const Index = () => {
+  return (
+    <footer>
+      <p>created by <a href="https://github.com/Muhammadrizo14">@ubuntuous</a></p>
+    </footer>
+  );
+};
+
+export default Index;
